f74f1f3b8147ec8b0b5a504d8dbe6740a80e8d64
Poorya Alishah <pooryaalishahkamandi14@gmail.com>
2024-08-06 16:16:22 +0330

    move yolov8n

--- predict.bash
+++ predict.bash
@@ -3,4 +3,5 @@
 # Prompt the user for input
 read -p "Image name: " imgName
 
+# Train command using yolo CLI
 yolo predict model=runs/detect/train8/weights/best.pt source="./../human-vehicles.v1-human-vehicles-zhivanai.yolov8/test/images/${imgName}.jpg" imgsz=640
